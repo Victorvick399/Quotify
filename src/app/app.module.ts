aea07f0da54b64e36b56670b21c91bc1020865a5
Victorvick399 <victorvick399@gmail.com>
2019-10-28 00:53:05 +0300

    add components to declarations of ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { ContactComponent } from './contact/contact.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { QuotelistComponent } from './quotelist/quotelist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    ContactComponent
+    ContactComponent,
+    ContactusComponent,
+    HomeComponent,
+    QuotelistComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
