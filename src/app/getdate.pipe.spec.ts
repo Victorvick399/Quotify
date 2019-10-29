2fba323742a41971a94f63a7aaaec259934be99f
Victorvick399 <victorvick399@gmail.com>
2019-10-29 09:14:00 +0300

    create getdate pipe

---
+++ src/app/getdate.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetdatePipe } from './getdate.pipe';
+
+describe('GetdatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetdatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
