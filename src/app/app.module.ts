a173cc5bc4b398a40a936cea71db10afe20e4ade
Victorvick399 <victorvick399@gmail.com>
2019-10-29 09:19:29 +0300

    connect all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { ContactComponent } from './contact/contact.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HomeComponent } from './home/home.component';
 import { QuotelistComponent } from './quotelist/quotelist.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighliterDirective } from './highliter.directive';
+import { GetdatePipe } from './getdate.pipe';;
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    ContactComponent,
     ContactusComponent,
     HomeComponent,
-    QuotelistComponent
+    QuotelistComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    HighliterDirective,
+    GetdatePipe
   ],
   imports: [
     BrowserModule,
